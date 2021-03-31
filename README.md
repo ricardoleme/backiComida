36f75b64bd5751c827b6e55485cf4938baca2849
Ricardo Leme <ricardo.leme@gmail.com>
2021-03-31 20:58:17 -0300

    Criação da Categoria

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Backend do Projeto iComida
+
+## Criando o projeto
+Criando o Backend
+=================
+cd projetos
+mkdir backicomida
+cd backicomida
+npm init (ENTER para o default)
+npm i express  (web server)
+npm i mongoose@5.11.15 (conexão com MongoDB)
+sudo npm i -g nodemon (Hot Reload)
+npm i dotenv (Variáveis de Ambiente)	
+code .
+
+Terminal/New Terminal
+git init
+git add .
+git commit -m "Projeto Inicial"
+node ou nodemon index.js
+## Mensagem de Erro: Nodemon: Error: listen EADDRINUSE: address already in use
+```
+$ lsof -i tcp:4000
+$ kill -9 PID
+``` 
--- routes/Categoria.js
+++ routes/Categoria.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult }  = require('express-validator')
+
 const Categoria = require('../model/Categoria')
 
 /*************************************
@@ -16,5 +18,99 @@ router.get('/', async(req, res)=> {
         })
     }
 })   
+
+/*************************************
+* Lista uma categoria pelo id
+* GET /categorias/:id
+**************************************/
+router.get('/:id', async(req, res)=> {
+    try{
+     const categoria = await Categoria.findById(req.params.id)
+     res.json(categoria)
+    }catch (err){
+        res.status(500).send({
+            errors: [{message: `Não foi possível obter a categoria com o id 
+                      ${req.params.id}`}]
+        })
+    }
+})  
+
+/*************************************
+* Inclui uma nova categoria
+* POST /categorias
+**************************************/
+const validaCategoria = [
+  check('nome', 'Nome da Categoria é obrigatório').not().isEmpty(),
+  check('status', 'Informe um status válido para a categoria.').isIn(['ativo','inativo'])
+]
+
+router.post('/', validaCategoria, 
+    async(req, res)=> {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+    //Verifica se a categoria já existe
+    const { nome } = req.body
+    let categoria = await Categoria.findOne({nome})
+    if (categoria)
+       return res.status(200).json({
+           errors: [{message: 'Já existe uma categoria com o nome informado!'}]
+       })
+    try{
+       let categoria = new Categoria(req.body)
+       await categoria.save()
+       res.send(categoria)
+    } catch (err){
+      return res.status(500).json({
+          errors: [{message: `Erro ao salvar a categoria: ${err.message}`}]
+      })
+    }  
+})
+
+/*************************************
+* Remove uma categoria
+* DELETE /categorias/:id
+**************************************/
+router.delete('/:id', async(req, res) => {
+    await Categoria.findByIdAndRemove(req.params.id)
+    .then(categoria => {
+        res.send({message: `Categoria ${categoria.nome} removida com sucesso!`})
+    }).catch(err => {
+        return res.status(500).send({
+      errors: [{message: `Não foi possível apagar a categoria com o id ${req.params.id}`}]
+        })
+    })
+})
+
+/*************************************
+* Edita a categoria
+* PUT /categorias
+**************************************/
+
+router.put('/', validaCategoria,
+          async(req, res)=> {
+          const errors = validationResult(req)
+          if (!errors.isEmpty()){
+              return res.status(400).json({
+                  errors: errors.array()
+              })
+          }
+          let dados = req.body
+          await Categoria.findByIdAndUpdate(req.body._id, {
+              $set: dados
+          }, {new: true})
+          .then(categoria => {
+              res.send({message: `Categoria ${categoria.nome} alterada com sucesso!`})
+          }).catch(err => {
+              return res.status(500).send({
+                  errors: [{message: 
+                 `Não foi possível alterar a categoria com o id ${req.body._id}`}]
+              })
+          })
+})
+
 
 module.exports = router
